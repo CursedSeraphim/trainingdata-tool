b5d3ece1b46727a18ec26688ff039f5631e65596
cursedseraphim <cat-noir@gmx.at>
2021-05-21 09:03:27 +0200

    add built files

---
+++ build/CMakeFiles/trainingdata-tool.dir/DependInfo.cmake
@@ -0,0 +1,102 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/adler32.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/adler32.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/compress.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/compress.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/crc32.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/crc32.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/deflate.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/deflate.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/gzclose.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/gzclose.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/gzlib.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/gzlib.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/gzread.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/gzread.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/gzwrite.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/gzwrite.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/infback.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/infback.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/inffast.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/inffast.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/inflate.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/inflate.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/inftrees.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/inftrees.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/trees.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/trees.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/uncompr.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/uncompr.c.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/zlib/zutil.c" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/zlib/zutil.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "NO_PEXT"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lc0/src"
+  "../lc0/src/chess"
+  "../lc0/src/neural"
+  "../polyglot/src"
+  "../zlib"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/lc0/src/chess/bitboard.cc" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/lc0/src/chess/bitboard.cc.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/lc0/src/chess/board.cc" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/lc0/src/chess/board.cc.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/lc0/src/chess/position.cc" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/lc0/src/chess/position.cc.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/lc0/src/neural/encoder.cc" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/lc0/src/neural/encoder.cc.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/lc0/src/neural/writer.cc" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/lc0/src/neural/writer.cc.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/lc0/src/utils/commandline.cc" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/lc0/src/utils/commandline.cc.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/lc0/src/utils/filesystem.posix.cc" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/lc0/src/utils/filesystem.posix.cc.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/lc0/src/utils/logging.cc" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/lc0/src/utils/logging.cc.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/lc0/src/utils/random.cc" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/lc0/src/utils/random.cc.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/attack.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/attack.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/board.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/board.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/book.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/book.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/book_merge.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/book_merge.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/colour.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/colour.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/fen.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/fen.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/game.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/game.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/hash.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/hash.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/io.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/io.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/line.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/line.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/list.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/list.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/move.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/move.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/move_do.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/move_do.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/move_gen.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/move_gen.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/move_legal.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/move_legal.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/option.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/option.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/parse.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/parse.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/pgn.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/pgn.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/piece.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/piece.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/random.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/random.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/san.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/san.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/square.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/square.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/polyglot/src/util.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/polyglot/src/util.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/src/PGNGame.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/src/PGNGame.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/src/PGNMoveInfo.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/src/PGNMoveInfo.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/src/TrainingDataDedup.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/src/TrainingDataDedup.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/src/TrainingDataReader.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/src/TrainingDataReader.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/src/TrainingDataWriter.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/src/TrainingDataWriter.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/src/polyglot_lib.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/src/polyglot_lib.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/src/trainingdata-tool.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/src/trainingdata-tool.cpp.o"
+  "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/src/trainingdata.cpp" "/home/cryptomancer/Desktop/work/chess_embedding/trainingdata-tool/build/CMakeFiles/trainingdata-tool.dir/src/trainingdata.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "NO_PEXT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lc0/src"
+  "../lc0/src/chess"
+  "../lc0/src/neural"
+  "../polyglot/src"
+  "../zlib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
